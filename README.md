7e57d1be114c5347bad81c9666c801ea218dcace
cheese <41850179+plainoldcheese@users.noreply.github.com>
2020-10-30 23:18:31 +0200

    use better js scrip for the calendar
    uses tables instead of hacky li tags

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 a simple calculator in vanilla javascript styled to resembe the GNU program gcal
 
-The source for the javascript comes from [this codepen](https://codesandbox.io/s/03kl637k7n?fontsize=14&file=/main.css:312-315) where the code was a copy of [this tutorial](https://dev.to/knheidorn/making-a-calendar-in-vanilla-javascript-48j8)
+I used a js calendar generator script i found [here](http://scripts.franciscocharrua.com/) and styled it
+
+I also added ``-prefers-colorscheme`` support
--- cal.js
+++ cal.js
@@ -1,4 +1,3 @@
-
 /* This script and many more are available free online at
 The JavaScript Source :: http://www.javascriptsource.com
 Created by: Francis Cocharrua :: http://scripts.franciscocharrua.com/ */
@@ -21,13 +20,13 @@ function calendar(date) {
   first_week_day = this_month.getDay();
   days_in_this_month = Math.round((next_month.getTime() - this_month.getTime()) / (1000 * 60 * 60 * 24));
 
-  calendar_html = '<table style="background-color:666699; color:ffffff;">';
-  calendar_html += '<tr><td colspan="7" style="background-color:9999cc; color:000000; text-align: center;">' + months[month] + ' ' + year + '</td></tr>';
+  calendar_html = '<table>';
+  calendar_html += '<tr><td id="header" colspan="7">' + months[month] + ' ' + year + '</td></tr>';
   calendar_html += '<tr>';
 
   // Fill the first week of the month with the appropriate number of blanks.
   for(week_day = 0; week_day < first_week_day; week_day++) {
-    calendar_html += '<td style="background-color:9999cc; color:000000;"> </td>';
+    calendar_html += '<td> </td>';
   }
 
   week_day = first_week_day;
@@ -38,9 +37,9 @@ function calendar(date) {
 
     // Do something different for the current day.
     if(day == day_counter)
-      calendar_html += '<td style="text-align: center;"><b>' + day_counter + '</b></td>';
+      calendar_html += '<td id="current-day"><b>' + day_counter + '</b></td>';
     else
-      calendar_html += '<td style="background-color:9999cc; color:000000; text-align: center;"> ' + day_counter + ' </td>';
+      calendar_html += '<td> ' + day_counter + ' </td>';
 
     week_day++;
   }
